54c78f2d69b97560565d625406fd27cdf73183e9
Patrick Rebel <patrick@7needs.nl>
2023-02-26 20:18:21 +0100

    Migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_25_213216) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_26_190240) do
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.string "location"
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "starts_at"
+    t.text "description"
   end
 
 end
