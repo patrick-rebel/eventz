bcd0711ca8e1044fa6254fddddbc404e9cdc6fb5
Patrick Rebel <patrick@7needs.nl>
2023-02-28 16:33:24 +0100

    The Flash

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,7 +15,7 @@ class EventsController < ApplicationController
   def update
     @event = Event.find(params[:id])
     if @event.update(event_params)
-      redirect_to @event
+      redirect_to @event, notice: "Event successfully updated!"
     else
       render :edit, status: :unprocessable_entity
     end
@@ -28,7 +28,7 @@ class EventsController < ApplicationController
   def create
     @event = Event.new(event_params)
     if @event.save
-      redirect_to @event
+      redirect_to @event, notice: "Event successfully created!"
     else
       render :new, status: :unprocessable_entity
     end
