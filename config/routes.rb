2656f285af93f5db5eb03d7357468e9f0a377b9f
Patrick Rebel <patrick@7needs.nl>
2023-03-01 17:09:11 +0100

    User Signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
   
   root "events#index"
   
   resources :events do
     resources :registrations
   end
+  
+  resources :users
+  get "signup" => "users#new"
 end
