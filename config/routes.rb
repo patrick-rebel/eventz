e69ba1b133441c94978d81bc1bcece1bd08f0813
Patrick Rebel <patrick@7needs.nl>
2023-03-01 20:50:58 +0100

    Sign In

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
     resources :registrations
   end
   
+  resource :session, only: [:new, :create, :destroy]
+  
   resources :users
   get "signup" => "users#new"
 end
